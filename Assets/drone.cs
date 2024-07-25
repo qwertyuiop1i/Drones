b4f445b1a82e75339dd80008e365dd63ccbcc7e2
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-25 19:58:53 -0400

    Commit #1

---
+++ Assets/drone.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drone : MonoBehaviour
+{
+    public float maxPower = 5;
+    public float maxTurnSpeed = 40;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+
+    }
+}
