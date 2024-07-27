4decbfb2b406af65d978df6bb191ddb9be6caa1c
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-27 13:49:32 -0400

    Commit #19

--- Assets/movingPoint.cs
+++ Assets/movingPoint.cs
@@ -7,12 +7,21 @@ public class movingPoint : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector2(0,-2);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+     
+            if (Input.GetMouseButtonDown(0)) { 
+   
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePosition.z = 0;
+           
+
+                transform.position = mousePosition;
+            }
         
     }
 }
